3847be7a953dc721b8306b31eab3d5ace618b636
ShiPeng <spone.2014@gmail.com>
2016-12-26 21:22:31 +0800

    token

--- tokenizer.js
+++ tokenizer.js
@@ -10,15 +10,47 @@ function Lexer(inputString) {
     } else {
         this.inputString = inputString;
     }
-
+    this.pointer = 0;
     //console.log(this.inputString);
+    function getLastChar() {
+        if (this.pointer<this.inputString.length())
+            return this.inputString[this.pointer++];
+        else return "EOF"
+    }
     function getToken() {
+        var identifierStr;
+        var numStr;
+        var lastChar=' ';
+        while (isspace(lastChar)) {
+            lastChar = getLastChar();
+        }
+        if (isalpha(lastChar)) {
+            identifierStr = lastChar;
+            while (isalnum(lastChar=getLastChar()))
+                identifierStr += lastChar;
 
+            if (identifierStr == "def") return "def";
+            if (identifierStr == "print") return "print";
+            return "tok";
+        }
+
+        if (isdigit(lastChar)) {
+            //Cannot support .91 => 0.91
+            do {
+                numStr += lastChar;
+                lastChar = getLastChar();
+            } while (isdigit(lastChar) || lastChar == '.')
+
+            return 'num';
+        }
+
+        if (lastChar == "#") {
+            do lastChar = getLastChar();
+            while (lastChar != '\n' && lastChar != '\r');
+            if (lastChar != "EOF") return getToken();
+        }
     }
 
-    function getNextToken() {
-        
-    }
 }
 
 
