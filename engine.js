93ea24ff6b8074632948d756575d030cc4a58440
fyhcastle <376317715@qq.com>
2016-12-27 20:05:23 +0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -88,4 +88,151 @@ function Engine() {
         }
     }
     
+    //simle stmt
+    function _exec_simple_stmt(ast, context) {
+        if (ast.type == "SIMLE_STMT") {
+            if (ast.sons.length == 1) {
+                if (ast.sons[0].type == "EXPR_STMT")
+                    _exec_expr_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "PRINT_STMT")
+                    _exec_print_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "DEL_STMT")
+                    _exec_del_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "PASS_STMT")
+                    _exec_pass_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "FLOW_STMT")
+                    _exec_flow_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "IMPORT_STMT")
+                    _exec_import_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "GLOBAL_STMT")
+                    _exec_global_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "EXEC_STMT")
+                    _exec_exec_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "ASSERT_STMT")
+                    _exec_assert_stmt(ast.sons[0], context);
+            }
+            else {
+                console.log("Simple Stmt Length Error");
+            }
+        }
+    }
+
+    function _exec_expr_stmt(ast, context) {
+        if (ast.type == "EXPR_STMT") {
+            if (ast.sons.length == 3) {
+                for (var key in ast.sons) {
+                    if (ast.sons[key].type == "TESTLIST")
+                        _exec_testlist(ast.sons[key], context);
+                    else if (ast.sons[key].type == "AUGASSIGN")
+                        _exec_augassign(ast.sons[key], context);
+                    else if (ast_sons[key].type == "=")
+                        x = 1;
+                }
+            }
+            else {
+                console.log("Expr Stmt Length Error");
+            }
+        }
+    }
+
+    function _exec_augassign(ast, context) {
+
+    }
+
+    function _exec_testlist(ast, context) {
+        if (ast.type == "TESTLIST") {
+            for (var key in ast.sons) {
+                if (ast.sons[key].type == "TEST")
+                    _exec_test(ast.sons[key], context);
+            }
+        }
+        return //
+    }
+
+
+    function _exec_test(ast, context) {
+        if (ast.type == "TEST") {
+            if (ast.sons.length == 1) {
+                if (ast.sons[0].type == "OR_TEST")
+                    _exec_or_test(ast.sons[0], context);
+            }
+            else {
+                console.log("Test Length Error");
+            }
+        }
+    }
+
+    function _exec_or_test(ast,context) {
+        if(ast.type == "OR_TEST"){
+            if(ast.sons[0].type == "AND_TEST")
+                _exec_and_test(ast.sons[0],context);
+           // if(ast.sons.length > 1)
+        }
+
+    }
+
+    function _exec_print_stmt(ast,context) {
+        if(ast.type == "PRINT_STMT"){
+            if(ast.sons.length == 2) {
+                if (ast.sons[1].type == "testlist")
+                    console.log("print" + _exec_testlist(ast.sons[1], context));
+            }
+            else {
+                console.log("Print_Stmt Length Error");
+            }
+        }
+    }
+
+    function _exec_del_stmt(ast,context) {
+        if(ast.type == "DEL_STMT"){
+            if(ast.sons.length == 2){
+                if(ast.sons[1].type == "EXPRLIST")
+                     _exec_exprlist(ast.sons[1],context);
+            }
+            else {
+                console.log("Del_Stmt Length Error");
+            }
+        }
+    }
+
+    function _exec_exprlist(ast,context) {
+        if (ast.type == "EXPRLIST") {
+            for (var key in ast.sons) {
+                if (ast.sons[key].type == "EXPR")
+                    _exec_expr(ast.sons[key], context);
+            }
+        }
+        return //
+    }
+
+    function _exec_expr(ast,context) {
+        if(ast.type == "EXPR"){
+
+        }
+    }
+
+    function _exec_pass_stmt(ast,context) {
+        if(ast.type == "PASS_STMT"){
+            console.log("pass");
+        }
+    }
+
+    function _exec_flow_stmt(ast,context){
+        if(ast.type == "FLOW_STMT"){
+            if(ast.sons.length == 1){
+                if(ast.sons[0].type =="BREAK_STMT")
+                    console.log("break");
+                else if(ast.sons[0].type == "CONTINUE_STMT")
+                    console.log("continue");
+                else if(ast.sons[0].type == "RETURN_STMT")
+                    _exec_return_stmt(ast.sons[0],context);
+                else if(ast.sons[0].type == "RAISE_STMT")
+                    _exec_raise_stmt(ast.sons[0],context);
+            }
+            else {
+                console.log("Flow_Stmt Length Error");
+            }
+        }
+    }
+    
 }
