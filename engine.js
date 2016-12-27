a832f52db2c32a7df6b57f329a159ec8e1b7a75d
ShiPeng <spone.2014@gmail.com>
2016-12-27 17:22:05 +0800

    update engine and BNF

--- engine.js
+++ engine.js
@@ -16,6 +16,19 @@ function Engine() {
     }
 
     function _exec_stmt(ast, context) {
+        if (ast.type == "STMT") {
+            if (ast.sons.length == 1) {
+                if (ast.sons[0].type == "SIMPLE_STMT")
+                    _exec_simple_stmt(ast.sons[0], context);
+                else if (ast.sons[0].type == "COMPOUND_STMT")
+                    _exec_simple_stmt(ast.sons[0], context);
+            } else {
+                console.log("Length Error");
+            }
+        }
+    }
 
+    function _exec_simple_stmt(ast, context) {
+        
     }
 }
