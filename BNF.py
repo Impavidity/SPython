2ee0ef002135aec0d8189b548bbc699d8f8208a8
ShiPeng <spone.2014@gmail.com>
2016-12-27 19:18:10 +0800

    update BNF

--- BNF.py
+++ BNF.py
@@ -1,22 +1,19 @@
 file_input: (stmt)*
-
-
 funcdef: 'def' NAME parameters ':' suite
 suite: simple_stmt | NEWLINE INDENT stmt+ DEDENT
 parameters: '(' [varargslist] ')'
+'''
 varargslist: ((fpdef ['=' test] ',')*
               ('*' NAME [',' '**' NAME] | '**' NAME) |
               fpdef ['=' test] (',' fpdef ['=' test])* [','])
 fpdef: NAME | '(' fplist ')'
 fplist: fpdef (',' fpdef)* [',']
-
+'''
 stmt: simple_stmt | compound_stmt
-simple_stmt: (expr_stmt | print_stmt  | del_stmt | pass_stmt | flow_stmt |
-             import_stmt | global_stmt | exec_stmt | assert_stmt)
-compound_stmt: if_stmt | while_stmt | for_stmt | try_stmt | with_stmt | funcdef | classdef | decorated
+simple_stmt: (expr_stmt | print_stmt  | del_stmt | pass_stmt | flow_stmt | import_stmt | global_stmt | exec_stmt | assert_stmt)
+compound_stmt: if_stmt | while_stmt | for_stmt | funcdef | classdef 
 expr_stmt: testlist augassign testlist | testlist '=' testlist
-augassign: ('+=' | '-=' | '*=' | '/=' | '%=' | '&=' | '|=' | '^=' |
-            '<<=' | '>>=' | '**=' | '//=')
+augassign: ('+=' | '-=' | '*=' | '/=' | '%=' | '&=' | '|=' | '^=' | '<<=' | '>>=' | '**=' | '//=')
 print_stmt: 'print' testlist
 del_stmt: 'del' exprlist
 pass_stmt: 'pass'
@@ -32,17 +29,8 @@ assert_stmt: 'assert' test [',' test]
 if_stmt: 'if' test ':' suite ('elif' test ':' suite)* ['else' ':' suite]
 while_stmt: 'while' test ':' suite 
 for_stmt: 'for' exprlist 'in' testlist ':' suite
-try_stmt: ('try' ':' suite
-           ((except_clause ':' suite)+
-            ['else' ':' suite]
-            ['finally' ':' suite] |
-           'finally' ':' suite))
-with_stmt: 'with' with_item (',' with_item)*  ':' suite
-with_item: test ['as' expr]
-except_clause: 'except' [test [('as' | ',') test]]
 
-
-test: or_test ['if' or_test 'else' test] | lambdef
+test: or_test
 or_test: and_test ('or' and_test)*
 and_test: not_test ('and' not_test)*
 not_test: 'not' not_test | comparison
@@ -55,27 +43,33 @@ shift_expr: arith_expr (('<<'|'>>') arith_expr)*
 arith_expr: term (('+'|'-') term)*
 term: factor (('*'|'/'|'%'|'//') factor)*
 factor: ('+'|'-'|'~') factor | power
-power: atom trailer* ['**' factor]
-atom: ('(' [testlist_comp] ')' |
-       '[' [listmaker] ']' |
-       '{' [dictorsetmaker] '}' |
+power: atom trailer* ['**' factor] # Not finished
+atom: ('(' [testlist_comp] ')' | # not finished
+       '[' [listmaker] ']' | #not finished
+       '{' [dictorsetmaker] '}' | # not finished
        '`' testlist1 '`' |
-       NAME | NUMBER | STRING+)
-trailer: '(' [arglist] ')' | '[' subscriptlist ']' | '.' NAME
+       NAME | NUMBER | STRING+)  
+trailer: '(' [arglist] ')' | '[' subscriptlist ']' | '.' NAME # not finished
 listmaker: test ( list_for | (',' test)* [','] )
 dictorsetmaker: ( (test ':' test (comp_for | (',' test ':' test)* [','])) |
-                  (test (comp_for | (',' test)* [','])) )
-testlist_comp: test ( comp_for | (',' test)* [','] )
-lambdef: 'lambda' [varargslist] ':' test
-
-subscriptlist: subscript (',' subscript)* [',']
-subscript: '.' '.' '.' | test | [test] ':' [test] [sliceop]
-sliceop: ':' [test]
+                  (test (comp_for | (',' test)* [','])) ) #not finished
+testlist_comp: test ( comp_for | (',' test)* [','] ) 
+classdef: 'class' NAME ['(' [testlist] ')'] ':' suite
 exprlist: expr (',' expr)* [',']
 testlist: test (',' test)* [',']
 
+list_iter: list_for | list_if
+list_for: 'for' exprlist 'in' testlist_safe [list_iter]
+list_if: 'if' old_test [list_iter]
 
-classdef: 'class' NAME ['(' [testlist] ')'] ':' suite
+# The following should be finished but I do not finish them currently
+subscriptlist: subscript (',' subscript)* [',']
+subscript: '.' '.' '.' | test | [test] ':' [test] [sliceop]
+sliceop: ':' [test]
+
+
+
+
 
 arglist: (argument ',')* (argument [',']
                          |'*' test (',' argument)* [',' '**' test] 
@@ -84,9 +78,7 @@ arglist: (argument ',')* (argument [',']
 # results in an ambiguity. ast.c makes sure it's a NAME.
 argument: test [comp_for] | test '=' test
 
-list_iter: list_for | list_if
-list_for: 'for' exprlist 'in' testlist_safe [list_iter]
-list_if: 'if' old_test [list_iter]
+
 
 testlist_safe: old_test [(',' old_test)+ [',']]
 old_test: or_test | old_lambdef
